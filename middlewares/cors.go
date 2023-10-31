d969b09b618853ff414dc3d20073aed8346fcc40
torbatti <torbattii@gmail.com>
2023-10-31 18:53:37 +0330

    initial

---
+++ middlewares/cors.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func Cors(router *chi.Mux) {
+
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+}
