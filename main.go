57f0bb0ca7770698cedcd5dfde0f18ac423d4446
torbatti <torbattii@gmail.com>
2023-11-01 18:01:17 +0330

    update: moved .env to init func

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/joho/godotenv"
 
 	"github.com/torbatti/nevise/apis"
@@ -19,6 +20,28 @@ import (
 )
 
 var app *core.App
+
+var PortString string
+var AUTH_SECRET string
+var JWT_SECRET string
+
+var tokenAuth *jwtauth.JWTAuth
+
+func init() {
+	// Loading .env Variables
+	godotenv.Load(".env") //os.Setenv(port, "8000")
+
+	// Getting .env Variables
+	PortString = os.Getenv("PORT")
+	AUTH_SECRET = os.Getenv("AUTH_SECRET")
+	JWT_SECRET = os.Getenv("JWT_SECRET")
+
+	middlewares.JWT_SECRET = JWT_SECRET
+
+	// TOKEN AUTH
+	tokenAuth = jwtauth.New("HS256", []byte(JWT_SECRET), nil)
+
+}
 
 func makeApp() *core.App {
 
@@ -44,32 +67,34 @@ func makeApp() *core.App {
 
 	return app
 }
+
 func main() {
 	app := makeApp()
-
-	// Setting Up .env
-	godotenv.Load(".env") //os.Setenv(port, "8000")
-	portString := os.Getenv("PORT")
-
-	// MiddleWares
-	middlewares.Cors(app.Router)
+	middlewares.Register()
 
 	// Root
 	root := chi.NewRouter()
+
+	root.Group(func(r chi.Router) {
+		// MiddleWares
+		middlewares.Cors(app.Router)
+
+		//Views
+		root.Get("/", routes.Index)
+		root.Get("/m-index", routes.MIndex)
+		root.Get("/m-signup", routes.MSignup)
+		root.Get("/m-login", routes.MLogin)
+
+		// Public
+		public := http.FileServer(http.Dir("./public"))
+		root.Mount("/", public)
+	})
 	app.Router.Mount("/", root)
-
-	// Routes
-	root.Get("/", routes.Index)
-	root.Get("/m-signup", routes.MSignup)
-
-	// Public
-	public := http.FileServer(http.Dir("./public"))
-	root.Mount("/", public)
 
 	// Initial server
 	server := &http.Server{
 		Handler: app.Router,
-		Addr:    ":" + portString,
+		Addr:    ":" + PortString,
 	}
 
 	// Listen And Serve
